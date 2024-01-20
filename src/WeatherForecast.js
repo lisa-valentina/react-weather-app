a037a951014b29ab72db7638d2e753d6d5809816
lisa-valentina <lisa.riedel@gmail.com>
2024-01-20 09:34:05 +0100

    receiving 401 error when including forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -4,40 +4,37 @@ import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
 
 export default function WeatherForecast(props) {
-  const [WeatherForecastData, setWeatherForecastData] = useState({
-    ready: false,
-  });
+  let [ready, setReady] = useState(false);
+  let [ForecastData, setForecastData] = useState(false);
 
   function handleResponse(response) {
-    setWeatherForecastData({
-      ready: true,
-      icon: response.data.weather[0].icon,
-      maxTemp: response.data.daily[0].temp[3],
-      minTemp: response.data.daily[0].temp[3],
-    });
+    setReady(true);
+    setForecastData(response.data.daily);
   }
-
-  function WeatherForecastDaily() {
-    const apiKey = "c6ccad355ceda3eadc4ad39e3b374f54";
-    let longitude = props.data.lon;
-    let latitude = props.data.lat;
-    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=current,minutely,hourly&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-  }
-
-  return (
-    <div className="WeatherForecast">
-      {WeatherForecastDaily}
-      <div className="row">
-        <div className="col">
-          <div className="WeatherForecast-day">Mon</div>
-          <WeatherIcon code="01d" size={40} />
-          <div className="WeatherForecast-temperatures">
-            <span className="WeatherForecast-max">{maxTemp}° </span>
-            <span className="WeatherForecast-min">3°</span>
+  if (ready) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <div className="WeatherForecast-day">{ForecastData[0].dt}</div>
+            <WeatherIcon code={ForecastData[0].weather[0].icon} size={40} />
+            <div className="WeatherForecast-temperatures">
+              <span className="WeatherForecast-max">
+                {ForecastData[0].temp.max}°{" "}
+              </span>
+              <span className="WeatherForecast-min">3°</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "c6ccad355ceda3eadc4ad39e3b374f54";
+    let longitude = props.data.lon;
+    let latitude = props.data.lat;
+    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
+    return null;
+  }
 }
